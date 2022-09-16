0ff74d828348bd088d9dc88df75558e3ead09d73
LautaroDerose <lautiderose@gmail.com>
2022-09-16 12:33:46 -0300

    focus formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,6 @@
 import React from 'react';
-// import './Formulario.css'
-
-
 import { useForm, ValidationError } from '@formspree/react';
 import Swal from 'sweetalert2';
-
-
 
 function ContactForm() {
   const [state, handleSubmit] = useForm("xoqbjkbl");
@@ -29,7 +24,6 @@ function ContactForm() {
         '<i class="fa fa-thumbs-up"></i> Ok!',
       confirmButtonAriaLabel: 'Thumbs up, great!'
     })
-    // <button onClick={showAlert}>Prueba Sweet</button>
   }
 
   return (
@@ -38,15 +32,13 @@ function ContactForm() {
         <form className=' justify-items-center rounded-md border-palette1 p-4 md:p-0 ' onSubmit={handleSubmit}>
           <div className='flex justify-center flex-wrap gap-4 '>
           <div className=' w-[100%] md:w-[45%]'>
-            {/* <label htmlFor="name">Nombre del emisario</label> */}
-            <input className='w-full mb-4 p-2 rounded-lg text-md text-palette1 bg-emerald-200'
+            <input className='w-full mb-4 p-2 rounded-lg text-md text-palette1 bg-emerald-200 focus:outline-none focus:shadow-md duration-300'
               id="name"
               type="text" 
               name="name"
               placeholder='Nombre del emisario'
             />
-            {/* <label htmlFor="email">Correo Electronico</label> */}
-            <input className='w-full my-1 p-2 rounded-lg text-md text-palette1 bg-emerald-200'
+            <input className='w-full my-1 p-2 rounded-lg text-md text-palette1 bg-emerald-200 focus:outline-none focus:shadow-md duration-300'
               id="email"
               type="email" 
               name="email"
@@ -58,18 +50,16 @@ function ContactForm() {
               field="email"
               errors={state.errors}
             />
-            {/* <label htmlFor="subject">Asunto</label> */}
-            <input className='w-full mt-4 p-2 rounded-lg text-md text-palette1 bg-emerald-200'
+            <input className='w-full mt-4 p-2 rounded-lg text-md text-palette1 bg-emerald-200 focus:outline-none focus:shadow-md duration-300'
               id="subject"
               type="text" 
               name="subject"
               placeholder='Asunto'
             />
-             {/* <button className=' bg-emerald-500 text-gray-900 font-medium w-full text-center rounded-md mt-4 py-3 ' type="submit" disabled={state.submitting} onClick={showAlert}>Enviar</button> */}
           </div>
           
           <div className=' w-[100%] md:w-[45%] '>
-            <textarea className='w-full min-h-[100px] h-full p-2 rounded-lg text-md text-palette1 bg-emerald-200 resize-none'
+            <textarea className='w-full min-h-[100px] h-full p-2 rounded-lg text-md text-palette1 bg-emerald-200 resize-none focus:outline-none focus:shadow-md duration-300'
             id="message"
             name="message"
             placeholder='Deje un mensaje :)'
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
         <div className='fixed w-full bg-gray-900 z-10 top-0  flex justify-between items-center  h-16  mx-auto px-4 md:px-[2rem] lg:px-[15%] text-white'>
           
-          <h1 className='w-full text-3xl font-bold text-[#34d399]'>React.</h1>
+          <h1 className='w-full text-xl font-bold text-[#34d399]'>&lt; Lautaro Derose /&gt;</h1>
           <ul className='hidden md:flex'>
             <li className='p-4 hover:text-emerald-500 '><button className='focus:outline-none focus:text-emerald-500'><Link to='/' spy={true} smooth={true} offset={50} duration={500}  >Home</Link></button></li>
             <li className='p-4 hover:text-emerald-500 '><button className='focus:outline-none focus:text-emerald-500'><Link to='formation' spy={true} smooth={true} offset={-100} duration={500}  >Formacion</Link></button></li>
@@ -27,7 +27,7 @@ const Navbar = () => {
           {nav ? <AiOutlineClose size={20} className='text-gray-400' /> : <AiOutlineMenu size={20} className='text-gray-400'/>}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-800 bg-gray-900 ease-in-out duration-500' : 'fixed left-[-100%] '}>
-            <h1 className='w-full text-3xl font-bold text-[#34d399] m-8'>React.</h1>
+            <h1 className='w-full text-lg font-bold text-[#34d399] m-8'>&lt; Lautaro Derose /&gt;</h1>
             <ul className='p-4 uppercase '>    
                     
                 <li className='p-4 text-gray-400 hover:text-emerald-500 border-b border-gray-600'><button className='focus:outline-none focus:text-emerald-500'><Link to='/' spy={true} smooth={true} offset={50} duration={500}  >Home</Link></button></li>
